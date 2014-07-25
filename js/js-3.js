65fba5453f57255e2cb8acecb6a30825b08db7fe
spring_raining <fdonthavei@gmail.com>
2014-07-26 02:26:21 +0900

    solve JavaScript exercises

--- js/js-3.js
+++ js/js-3.js
@@ -1,1 +1,12 @@
 // 課題 JS-3 の実装をここに記述してください。
+(function() {
+    var submitButton = document.getElementById("submit-button");
+    var tableContainer = document.getElementById("table-container");
+
+    submitButton.addEventListener("click", function() {
+        tableContainer.textContent = "";
+        var text = document.getElementById("log-input").value;
+        var parsed = parseLTSVLog(text);
+        createLogTable(tableContainer, parsed);
+    });
+})();
--- js/main.js
+++ js/main.js
@@ -1,3 +1,66 @@
 // 課題 JS-1: 関数 `parseLTSVLog` を記述してください
+function parseLTSVLog(logStr) {
+    var re = /^path:(.+)\treqtime_microsec:(\d+)$/;
+    var logArray = logStr.split("\n");
+    var rtn = []
+
+    for (var i=0; i < logArray.length; i++) {
+        var match = logArray[i].match(re);
+        if (!match)
+            continue;
+        rtn.push({
+            path: match[1],
+            reqtime_microsec: Number(match[2], 10),
+        });
+    }
+
+    return rtn;
+}
 
 // 課題 JS-2: 関数 `createLogTable` を記述してください
+function createLogTable(domElem, logArray) {
+    var table = document.createElement("table");
+    var tbody = document.createElement("tbody");
+
+    table.appendChild((function() {
+        var thead = document.createElement("thead");
+        thead.appendChild((function() {
+            var tr = document.createElement("tr");
+            tr.appendChild((function() {
+                var th = document.createElement("th");
+                th.textContent = "path";
+                return th;
+            })());
+            tr.appendChild((function() {
+                var th = document.createElement("th");
+                th.textContent = "reqtime_microsec";
+                return th;
+            })());
+            return tr;
+        })());
+        return thead;
+    })());
+
+    for (var i=0; i < logArray.length; i++) {
+        var log = logArray[i];
+
+        tbody.appendChild((function() {
+            var tr = document.createElement("tr");
+            tr.appendChild((function() {
+                var td = document.createElement("td");
+                td.textContent = log.path;
+                return td;
+            })());
+            tr.appendChild((function() {
+                var td = document.createElement("td");
+                td.textContent = String(log.reqtime_microsec)
+                return td;
+            })());
+            return tr;
+        })());
+    }
+
+    table.appendChild(tbody);
+    domElem.appendChild(table);
+    return domElem;
+}
